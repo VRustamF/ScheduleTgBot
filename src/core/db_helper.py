17f694cb5e6f631a3a64cc38e9dfaec7e4747785
Rustam <rustamvurdihanov@gmail.com>
2026-02-16 00:02:39 +0700

    Помощник для работы с бд

---
+++ src/core/db_helper.py
@@ -0,0 +1,49 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    AsyncEngine,
+    async_sessionmaker,
+)
+
+from core.config import settings
+
+
+class DataBaseHelper:
+    """Класс-помощник для работы с БД"""
+
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        max_overflow: int = 10,
+        pool_size: int = 5,
+    ):
+        """Инициализация движка и фабрики сессий"""
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            max_overflow=max_overflow,
+            pool_size=pool_size,
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            expire_on_commit=False,
+            autoflush=False,
+            autocommit=False,
+        )
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        """Создаёт сессию для работы с БД. После выполнения работы сессия автоматически закрывается"""
+        async with self.session_factory() as session:
+            yield session
+
+    async def dispose(self):
+        """Закрывает соединения с БД"""
+        await self.engine.dispose()
+
+
+db_helper = DataBaseHelper(url=str(settings.db.url))
